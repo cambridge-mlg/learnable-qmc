ec0124b8a3997ef49f03fd7e75749e0c72ee96d6
stratisMarkou <stratismar@gmail.com>
2024-02-13 14:36:56 +0000

    copula forward works

---
+++ lqmc/copula.py
@@ -0,0 +1,64 @@
+from typing import Callable
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+tfk = tf.keras
+tfd = tfp.distributions
+tfb = tfp.bijectors
+
+from lqmc.random import Seed, randn, mvn_chol
+
+
+class GaussianCopula(tfk.Model):
+    def __init__(
+        self,
+        seed: Seed,
+        dim: int,
+        target_inverse_cdf: Callable,
+        name="gaussian_copula",
+        **kwargs,
+    ):
+
+        super().__init__(name=name, **kwargs)
+
+        self.dim = dim
+        num_thetas = 2 * dim * (2 * dim - 1) // 2
+        _, thetas_init = randn(
+            (num_thetas,),
+            seed=seed,
+            mean=tf.zeros((num_thetas), dtype=self.dtype),
+            stddev=tf.ones((num_thetas), dtype=self.dtype),
+        )
+        self.thetas = tf.Variable(thetas_init, dtype=float)
+        self.bijector = tfp.bijectors.CorrelationCholesky()
+        self.target_inverse_cdf = target_inverse_cdf
+
+    @property
+    def cholesky(self) -> tf.Tensor:
+        return self.bijector(self.thetas)
+
+    @property
+    def covariance(self) -> tf.Tensor:
+        return tf.matmul(self.cholesky, self.cholesky, transpose_b=True)
+
+    def call(
+        self,
+        seed: Seed,
+        batch_size: int,
+    ) -> tf.Tensor:
+
+        seed, samples = mvn_chol(
+            seed=seed,
+            mean=tf.ones((batch_size, 2 * self.dim)),
+            cov_chol=self.cholesky,
+        )
+
+        norm = tfd.Normal(
+            loc=tf.zeros((), dtype=self.dtype),
+            scale=tf.ones((), dtype=self.dtype),
+        )
+        samples = self.target_inverse_cdf(norm.cdf(samples))
+
+        return seed, samples
