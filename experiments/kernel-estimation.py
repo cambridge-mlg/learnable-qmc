bf5b76400bf51c672f088dcaa98f59e127cb7dec
stratisMarkou <stratismar@gmail.com>
2024-04-09 17:24:20 +0100

    experiments running

--- experiments/kernel-estimation.py
+++ experiments/kernel-estimation.py
@@ -1,12 +1,16 @@
 import argparse
+import os
 
 from tqdm import trange
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 from lqmc.joint import (
     IndependentUniform,
     HaltonSequence,
     GaussianCopulaAntiparallelCorrelated,
+    GaussianCopulaAntiparallelUncorrelated,
     GaussianCopulaAntiparallelAnticorrelated,
     GaussianCopulaParametrised,
 )
@@ -24,6 +28,9 @@ def parse_args():
     # Make argument parser with just the config argument
     parser = argparse.ArgumentParser()
 
+    # Results arguments
+    parser.add_argument("--results-path", type=str, default="_results")
+
     # Dataset arguments
     parser.add_argument("--dataset", type=str)
     parser.add_argument("--seed-dataset", type=int)
@@ -40,31 +47,71 @@ def parse_args():
 
     # Joint sampler arguments
     parser.add_argument("--num-ensembles", type=int)
+    parser.add_argument("--num-trials", type=int)
 
     # Training arguments
     parser.add_argument("--seed-training", type=int)
     parser.add_argument("--num-steps", type=int)
+    parser.add_argument("--sampler-num-steps", type=int)
     parser.add_argument("--learning-rate", type=float)
+    parser.add_argument("--sampler-learning-rate", type=float)
 
     return parser.parse_args()
 
 
 @tf.function
 def gradient_step(optimizer: tf.keras.optimizers.Optimizer, model: tf.keras.Model):
-    
+
     with tf.GradientTape() as tape:
         loss = model.loss()
-    
+
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
     return loss
 
 
+@tf.function
+def joint_sampler_gradient_step(
+    seed: tf.Tensor,
+    optimizer: tf.keras.optimizers.Optimizer,
+    joint_sampler: tf.keras.Model,
+    kernel: tf.keras.Model,
+    x: tf.Tensor,
+    num_ensembles: int,
+):
+
+    with tf.GradientTape() as tape:
+
+        seed, omega = joint_sampler(
+            seed=seed,
+            batch_size=num_ensembles,
+        )
+        seed, loss = kernel.rmse_loss(
+            seed=seed,
+            omega=omega,
+            x1=x,
+            num_ensembles=num_ensembles,
+        )
+
+    gradients = tape.gradient(loss, joint_sampler.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, joint_sampler.trainable_variables))
+
+    return seed, loss
+
+
 def main():
 
     # Parser arguments
     args = parse_args()
+
+    # Create directories if they don't exist
+    results_path = os.path.join(
+        args.results_path,
+        args.dataset,
+        f"split_{args.split_id}_{args.num_splits}",
+    )
+    os.makedirs(results_path, exist_ok=True)
 
     # Create dataset
     dataset = make_dataset(
@@ -93,30 +140,106 @@ def main():
         dtype=DTYPE,
     )
 
-    # Create random feature GP
-    rfgp = RandomFeatureGaussianProcess(
-        joint_sampler=GaussianCopulaParametrised(
-            seed=args.seed_training,
+    copula_seed = [args.seed_training, args.seed_training]
+    joint_samplers = {
+        "iid": IndependentUniform(dim=dataset.dim, dtype=DTYPE),
+        "halton": HaltonSequence(dim=dataset.dim, dtype=DTYPE),
+        "ortho": GaussianCopulaAntiparallelUncorrelated(dim=dataset.dim, dtype=DTYPE),
+        "anti": GaussianCopulaAntiparallelAnticorrelated(dim=dataset.dim, dtype=DTYPE),
+        "learnt": GaussianCopulaParametrised(
+            seed=copula_seed,
             dim=dataset.dim,
-            trainable=True,
             dtype=DTYPE,
         ),
-        kernel=kernel,
-        noise_std=args.noise_std,
-        x=dataset.x_train,
-        y=dataset.y_train,
-        dtype=DTYPE,
-    )
+    }
 
     # Create optimizer
     optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
 
-    # Train GP hyperparameters -- add a progress bar with the loss
+    # Train GP hyperparameters
     pbar = trange(args.num_steps)
-    for i in pbar:
+    for _ in pbar:
         loss = gradient_step(optimizer, gp)
         pbar.set_description(f"GP loss: {loss.numpy():.4f}")
 
+    # Evaluate RMSE and NLL on test set
+    mean, _ = gp(dataset.x_test, noiseless=False)
+    rmse = tf.reduce_mean((mean - dataset.y_test) ** 2.0) ** 0.5
+    nll = gp.pred_nll(dataset.x_test, dataset.y_test)
+
+    # Print prediction metrics and save them into a text file
+    print(f"RMSE: {rmse.numpy()}")
+    print(f"NLL: {nll.numpy()}")
+    with open(os.path.join(results_path, "prediction-metrics"), "w") as file:
+        file.write(f"RMSE: {rmse.numpy()}\n")
+        file.write(f"NLL: {nll.numpy()}\n")
+
+    # Print trained parameters and save them into a text file
+    print(f"GP lengthscales: {gp.kernel.lengthscales.numpy()}")
+    print(f"GP output scale: {gp.kernel.output_scale.numpy()}")
+    print(f"GP noise std: {gp.noise_std.numpy()}")
+
+    with open(os.path.join(results_path, "trained-hypers"), "w") as file:
+        file.write(f"lengthscales: {gp.kernel.lengthscales.numpy()}\n")
+        file.write(f"output_scale: {gp.kernel.output_scale.numpy()}\n")
+        file.write(f"noise_std: {gp.noise_std.numpy()}\n")
+
+    if os.path.exists(os.path.join(results_path, f"joint-metrics")):
+        os.remove(os.path.join(results_path, f"joint-metrics"))
+
+    # Re-create optimizer
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.sampler_learning_rate)
+    
+    # Train learnt joint sampler
+    pbar = trange(args.sampler_num_steps)
+    seed = [args.seed_training, args.seed_training]
+    sampler_losses = []
+    for _ in pbar:
+        seed, loss = joint_sampler_gradient_step(
+            optimizer=optimizer,
+            joint_sampler=joint_samplers["learnt"],
+            kernel=kernel,
+            x=dataset.x_train,
+            num_ensembles=args.num_ensembles,
+            seed=seed,
+        )
+        sampler_losses.append(loss)
+        pbar.set_description(f"Learnt sampler loss: {tf.reduce_mean(sampler_losses).numpy():.4f}")
+
+    # Loop over joints
+    for name, joint in joint_samplers.items():
+        mse_losses = []
+        for _ in trange(args.num_trials):
+            seed, omega = joint(seed, args.num_ensembles)
+            seed, rmse_loss = kernel.rmse_loss(
+                seed=seed,
+                omega=omega,
+                x1=dataset.x_train,
+                num_ensembles=args.num_ensembles,
+            )
+            mse_losses.append(rmse_loss)
+
+        # Print mean and stderr of MSE losses
+        mse_losses = tf.stack(mse_losses)
+        mean_mse_loss = tf.reduce_mean(mse_losses)
+        stderr_mse_loss = tf.math.reduce_std(mse_losses) / args.num_trials**0.5
+
+        print(f"\n{name} MSE loss: {mean_mse_loss.numpy()} +/- {stderr_mse_loss.numpy()}\n")
+
+        # Save mean and stderr of MSE losses into a text file
+        with open(os.path.join(results_path, f"joint-metrics"), "a") as file:
+            file.write(
+                f"{name} MSE: {mean_mse_loss.numpy()} +/- {stderr_mse_loss.numpy()}\n"
+            )
+        
+        # Save learnt sampler covariance as numpy and imshow
+        if name == "learnt":
+            cov = joint.covariance.numpy()
+            np.save(os.path.join(results_path, "learnt-covariance.npy"), cov)
+
+            plt.imshow(cov)
+            plt.axis("off")
+            plt.savefig(os.path.join(results_path, "learnt-covariance.png"))
 
 
 if __name__ == "__main__":
---
+++ experiments/run-all-kernel-estimation.py
@@ -0,0 +1,45 @@
+from subprocess import call
+from data.datasets import DATASETS
+
+
+num_splits = 5
+
+for dataset in DATASETS.keys():
+    for split_id in range(num_splits):
+
+        call(
+            [
+                "python",
+                "experiments/kernel-estimation.py",
+                "--dataset",
+                dataset,
+                "--seed-dataset",
+                "0",
+                "--num-splits",
+                str(num_splits),
+                "--split-id",
+                str(split_id),
+                "--max-datapoints",
+                "128",
+                "--lengthscale",
+                "1.0",
+                "--output-scale",
+                "1.0",
+                "--noise-std",
+                "0.1",
+                "--num-ensembles",
+                "1",
+                "--seed-training",
+                str(split_id),
+                "--num-steps",
+                "5000",
+                "--learning-rate",
+                "0.01",
+                "--num-trials",
+                "100",
+                "--sampler-learning-rate",
+                "0.001",
+                "--sampler-num-steps",
+                "1000",
+            ]
+        )
